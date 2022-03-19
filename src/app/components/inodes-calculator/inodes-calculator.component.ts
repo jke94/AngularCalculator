cfd7ff4dd3de79a391f1420512d07737a4193d53
jke94 <jcarracedo.dev@gmail.com>
2022-03-19 12:07:09 +0100

    Add basic inode calculator schema.

--- src/app/components/inodes-calculator/inodes-calculator.component.ts
+++ src/app/components/inodes-calculator/inodes-calculator.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InodesCalculatorComponent implements OnInit {
 
+  nDirectPointers : number = 0;
+  nSimpleDirectPointers : number = 0;
+  nDoubleDirectPointers : number = 0;
+  nTripleDirectPointers : number = 0;
+  clusterSize : number = 0;
+  directionSize : number = 0;
+  nDirByBlock : number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  Calculate() : void
+  {
+    this.nDirByBlock = this.clusterSize / this.directionSize
+  }
+  Reset() : void
+  {
+    this.nDirectPointers = 0
+    this.nSimpleDirectPointers = 0
+    this.nDoubleDirectPointers = 0
+    this.nTripleDirectPointers = 0
+    this.clusterSize = 0
+    this.directionSize = 0
+    this.nDirByBlock = 0
+  }
 }
